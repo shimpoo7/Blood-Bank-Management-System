43f77c52384a2e07eec1e33b1b32f56842451711
sudokhushidave <74859134+sudokhushidave@users.noreply.github.com>
2021-05-28 13:44:02 +0530

    Update donor.sql

--- donor.sql
+++ donor.sql
@@ -106,7 +106,6 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
-
 --
 -- Dumping data for table `users`
 --

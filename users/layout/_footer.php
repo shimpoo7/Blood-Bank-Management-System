1df4fbe0e23accf1c0d1016fba24c2d6a93b82ae
shimpoo7 <46793608+shimpoo7@users.noreply.github.com>
2021-05-28 13:24:12 +0530

    Update _footer.php

--- users/layout/_footer.php
+++ users/layout/_footer.php
@@ -1,7 +1,5 @@
 <footer class="nav navbar-fixed-bottom">
-            <b>Made By: <a href="http://projectworlds.in">Yugesh Verma</a></b>
-            <br>
-            &COPY;projectworlds
+            
 </footer>
 
 
